1296c5c27f02a8efa591cacd17776107f23d1a7e
Suraj Rane <surajrane42@gmail.com>
2020-05-17 14:19:32 +0530

    backup for failure of env variables

--- app.js
+++ app.js
@@ -16,11 +16,8 @@ var commentRoutes = require("./routes/comments"),
   campgroundRoutes = require("./routes/campgrounds"),
   indexRoutes = require("./routes/index");
 
-// mongoose.connect("mongodb://localhost:27017/yelp_camp", {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true
-// });
-mongoose.connect(process.env.DATABASEURL, {
+var url = process.env.DATABASEURL || "mongodb://localhost:27017/yelp_camp";
+mongoose.connect(url, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
